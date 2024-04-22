5302aeaad8f626c6f55b70f3ba0c86811f41612a
Onyenze Chetachi <chetaGod4life@gmail.com>
2024-04-22 20:10:58 +0100

    typ

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -2,6 +2,7 @@
 '''annotate a given function'''
 from typing import Iterable, Sequence, List, Tuple
 
+
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     '''annotatting'''
     return [(i, len(i)) for i in lst]
