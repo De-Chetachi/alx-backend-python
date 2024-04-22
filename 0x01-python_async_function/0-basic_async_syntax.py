3ad45bb387ae09e489bdd2e75bc1065bf9609e53
Onyenze Chetachi <chetaGod4life@gmail.com>
2024-04-22 22:30:45 +0100

    it

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -2,9 +2,10 @@
 '''a module'''
 import asyncio
 import random
+from typing import Union
 
 
-async def wait_random(max_delay: int = 10):
+async def wait_random(max_delay: int = 10) -> Union[int, float]:
     '''what is this?'''
     ran = random.uniform(0, max_delay)
     await asyncio.sleep(ran)
