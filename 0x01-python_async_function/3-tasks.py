50f8a837591ce658669e936507b0c30dab142468
Onyenze Chetachi <chetaGod4life@gmail.com>
2024-04-25 03:35:35 +0100

    type

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -2,10 +2,10 @@
 '''a module'''
 import asyncio
 import random
-from typing import Union, List, Generic
+from typing import Union, List, Awaitable
 wait_r = __import__('0-basic_async_syntax').wait_random
 
 
-def task_wait_random(max_delay: int) -> Generic:
+def task_wait_random(max_delay: int) -> Awaitable:
     '''hmm oku'''
     return asyncio.create_task(wait_r(max_delay))
