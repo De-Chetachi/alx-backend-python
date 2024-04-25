b38735701eef2044a1e8ac26294e074ac2d57d4c
Onyenze Chetachi <chetaGod4life@gmail.com>
2024-04-25 02:50:35 +0100

    concur

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -6,7 +6,7 @@ from typing import Union, List
 wait_r = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> List[float]]:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     '''hmm oku'''
-    delays = await asyncio.gather(*(wait_r(max_delay) for i in range(n)))
-    return delays
+    delays = [asyncio.create_task(wait_r(max_delay)) for i in range(n)]
+    return [await delay for delay in asyncio.as_completed(delays)]
