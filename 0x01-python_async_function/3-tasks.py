9befc6d3f4c403b7f8f3ac6c3910071105b884f2
Onyenze Chetachi <chetaGod4life@gmail.com>
2024-04-25 03:50:14 +0100

    type

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -6,6 +6,6 @@ from typing import Union, List, Awaitable
 wait_r = __import__('0-basic_async_syntax').wait_random
 
 
-def task_wait_random(max_delay: int) -> Awaitable:
+def task_wait_random(max_delay: int) -> asyncio.Task:
     '''hmm oku'''
     return asyncio.create_task(wait_r(max_delay))
