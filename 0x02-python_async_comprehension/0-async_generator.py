6062168bab0ad17424bbf17db097e7a81e3ab9fb
Onyenze Chetachi <chetaGod4life@gmail.com>
2024-04-29 00:24:32 +0100

    typr

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -2,9 +2,9 @@
 '''a module ith asyn await'''
 import asyncio
 import random
+from typing import Generator
 
-
-async def async_generator() -> float:
+async def async_generator() -> Generator[float, None, None]:
     '''async function'''
     for i in range(10):
         await asyncio.sleep(1)
